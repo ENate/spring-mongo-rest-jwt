cfad3ab9b2a7faa30c85dec7d6ca1f04468ad893
Nath <nathaniel@aims.ac.za>
2022-07-02 08:16:08 +0000

    Created a spring parent app

---
+++ spring-jwt/src/test/java/com/minejava/springjwt/SpringJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minejava.springjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
