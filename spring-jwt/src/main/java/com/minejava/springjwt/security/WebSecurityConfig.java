b76a0f7bc2208c1266229f921e63ced271ce1f4d
Nath <nathaniel@aims.ac.za>
2022-07-04 14:25:00 +0000

    Fixed websecurityconfig via securityfilterchain

--- spring-jwt/src/main/java/com/minejava/springjwt/security/WebSecurityConfig.java
+++ spring-jwt/src/main/java/com/minejava/springjwt/security/WebSecurityConfig.java
@@ -9,7 +9,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -25,75 +25,69 @@ import lombok.RequiredArgsConstructor;
 @EnableWebSecurity
 @RequiredArgsConstructor
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig { 
+    //  extends WebSecurityConfigurerAdapter 
     @Autowired
     private UserService userService;
     @Autowired
     private AccessTokenEntryPoint accessTokenEntryPoint;
 
-    private final AuthenticationConfiguration configuration;
-
+    
+    AuthenticationManager authenticationManager;
     
     @Bean
     public AccessTokenFilter accessTokenFilter() {
         return new AccessTokenFilter();
     }
 
-    
-     
     @Bean
-    @Override
-    protected AuthenticationManager authenticationManager() throws Exception {
-        return super.authenticationManager();
-    }
-    
-    // @Bean
-    // protected AuthenticationManager authenticationManager() throws Exception {
-    //     return configuration.getAuthenticationManager();
-    // }
-    
-   
+        public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+            return authenticationConfiguration.getAuthenticationManager();
+        }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
- 
-    // @Bean
-    // public BCryptPasswordEncoder passwordEncoder() {
-    //     return new BCryptPasswordEncoder();
-    // }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userService)
-                .passwordEncoder(passwordEncoder());
-    }
-    // @Autowired
-    // protected void configure(AuthenticationManagerBuilder builder) throws Exception {
-    //     builder.userDetailsService(userService).passwordEncoder(new BCryptPasswordEncoder());
-    // }
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userService);
+        authenticationManager = authenticationManagerBuilder.build();
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable()
-                .exceptionHandling().authenticationEntryPoint(accessTokenEntryPoint).and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .authorizeRequests().antMatchers("/api/auth/**").permitAll()
-                .anyRequest().authenticated();
-        http.addFilterBefore(accessTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+        http.csrf().disable().cors().disable()
+                    .exceptionHandling().authenticationEntryPoint(accessTokenEntryPoint).and()
+                    .authenticationManager(authenticationManager)
+                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                    .authorizeRequests().antMatchers("/api/auth/**").permitAll()
+                    .anyRequest().authenticated();
+            http.addFilterBefore(accessTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
     }
 
-    
     // @Bean
-    // public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    // @Override
+    // protected AuthenticationManager authenticationManager() throws Exception {
+    //     return super.authenticationManager();
+    // }
+
+    // @Override
+    // protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    //     auth.userDetailsService(userService)
+    //             .passwordEncoder(passwordEncoder());
+    // }
+
+    // @Override
+    // protected void configure(HttpSecurity http) throws Exception {
     //     http.cors().and().csrf().disable()
-    //         .exceptionHandling().authenticationEntryPoint(accessTokenEntryPoint).and()
-    //         .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-    //         .authorizeRequests().antMatchers("/api/auth**").permitAll()
-    //         .anyRequest().authenticated();
-            
-    //         http.addFilterBefore(accessTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-    //         return http.build();
+    //             .exceptionHandling().authenticationEntryPoint(accessTokenEntryPoint).and()
+    //             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+    //             .authorizeRequests().antMatchers("/api/auth/**").permitAll()
+    //             .anyRequest().authenticated();
+    //     http.addFilterBefore(accessTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     // }
+
+
 }
